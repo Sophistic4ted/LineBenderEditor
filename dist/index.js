f09c0ea86a240f40a646784daf6d0267e8e70b22
Soph <sophistic4ted@gmail.com>
2023-05-24 22:18:08 +0200

    Hide the grid outside the canvas.

--- dist/index.js
+++ dist/index.js
@@ -1,24 +1,26 @@
-import { TileSelector } from "./TileSelector.js";
 import { SpriteLoader } from "./SpriteLoader.js";
-// Load the sprites
-const spriteLoader = new SpriteLoader('sprites_2.png', 28);
-spriteLoader.loadSprites([
+import { TileSelector } from "./TileSelector.js";
+import { GridEditor } from "./GridEditor.js"; // Assuming you've created a GridEditor.ts file
+const spriteSheetUrl = "sprites_2.png";
+const tileSize = 28; // Use the size that matches your sprites
+const gridWidth = 100; // Choose a grid size that suits your needs
+const gridHeight = 100;
+// Define the tile types and their positions in the sprite sheet
+const spriteData = [
     { id: 'G', xIndex: 0, yIndex: 0 },
     { id: 'B', xIndex: 0, yIndex: 1 },
     { id: 'S', xIndex: 0, yIndex: 2 },
     { id: 'D', xIndex: 0, yIndex: 4 },
     { id: 'K', xIndex: 0, yIndex: 5 },
     { id: 'W', xIndex: 0, yIndex: 6 }
-    // Add more sprite indices as needed...
-]).then(() => {
-    // Define the tile types
-    const tileTypes = ['G', 'B', 'W', 'S', 'K', 'D'];
-    // Load the sprites
-    // Create a new TileSelector instance
+    // Add more tile types as needed
+];
+const spriteLoader = new SpriteLoader(spriteSheetUrl, tileSize);
+spriteLoader.loadSprites(spriteData).then(() => {
+    const tileTypes = spriteData.map(data => data.id);
     const tileSelector = new TileSelector(spriteLoader, tileTypes);
-    // Now you can get the selected tile type whenever you need it
-    console.log('Selected tile type:', tileSelector.getSelectedTile());
-    // TODO: Use the selected tile type for drawing tiles, etc.
-}).catch(error => {
-    console.error('Failed to load sprites:', error);
+    // Create the grid editor
+    const gridEditor = new GridEditor(spriteLoader, tileSelector, tileSize, gridWidth, gridHeight);
+}).catch((error) => {
+    console.error("Failed to load sprites:", error);
 });
